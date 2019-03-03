5a1a3f7deb5daad4b8ccf1d84a3424cb8b840f94
Rashmi206 <rashmisah20june@gmail.com>
2019-03-03 23:07:58 +0530

    Config changes

--- ProjectManagerFrontend/src/karma.conf.js
+++ ProjectManagerFrontend/src/karma.conf.js
@@ -26,6 +26,18 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     proxies: {
       '/api': {
